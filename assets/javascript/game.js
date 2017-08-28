60eabc2369fa7e9261a996baa9dec1a64f9e065e
rcintron1 <rolando.cintron@gmail.com>
2017-08-28 07:22:13 -0400

    matching site to requirements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function() {
   console.log("function->ready()")
   // $("div").html("Rolo");
+  // var varDiv = $("<div></div>");
+
+  for (i = 1; i < 5; i++) {
+    var varDiv = $("<div></div>");
+    varDiv.addClass("col-sm-1");
+    link = '<img src="assets/images/' + i + '.jpg">';
+    varDiv.append(link);
+    console.log(varDiv);
+    $("#div_gems").append(varDiv);
+  };
 });
