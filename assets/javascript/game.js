59c20dd8909017b89ed82a497d54a1239b378f19
Rolando Cintron <rolando.cintron@gmail.com>
2017-08-28 23:49:59 -0400

    added images using jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,19 @@ $(document).ready(function() {
   console.log("function->ready()")
   // $("div").html("Rolo");
   // var varDiv = $("<div></div>");
-
+  var link = "";
+  var varDiv = "";
   for (i = 1; i < 5; i++) {
-    var varDiv = $("<div></div>");
+    varDiv = $('<div id="' + i + '">');
     varDiv.addClass("col-sm-1");
     link = '<img src="assets/images/' + i + '.jpg">';
     varDiv.append(link);
+    varDiv.on("click",imgClick);
     console.log(varDiv);
     $("#div_gems").append(varDiv);
   };
 });
+
+function imgClick(e){
+  console.log("imageClick",this.id);
+}
