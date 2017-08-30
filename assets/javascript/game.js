fd5d500d6d2ce580bf28e2df5cf58af47d048e15
rcintron1 <rolando.cintron@gmail.com>
2017-08-29 23:28:57 -0400

    fixed some of the javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,27 @@ $(document).ready(function() {
   // var varDiv = $("<div></div>");
   var link = "";
   var varDiv = "";
-  for (i = 1; i < 5; i++) {
-    varDiv = $('<div id="' + i + '">');
+  for (i = 0; i < 4; i++) {
+    crystalId = "img" + (i + 1);
+    crystal.charPool[i]= crystalId;
+    varDiv = $('<div id="' + crystalId + '">');
     varDiv.addClass("col-sm-1");
-    link = '<img src="assets/images/' + i + '.jpg">';
+    link = '<img src="assets/images/' + crystalId + '.jpg">';
     varDiv.append(link);
-    varDiv.on("click",imgClick);
-    console.log(varDiv);
+    varDiv.on("click",crystal.imgClick);
+    // console.log(varDiv);
     $("#div_gems").append(varDiv);
   };
 });
 
-function imgClick(e){
-  console.log("imageClick",this.id);
+var crystal = {
+  charPool:[],
+  imgClick:function (e){
+    console.log("imageClick",this.id);
+    crystal.divHandler(this.id);
+  },
+  divHandler:function (e){
+    console.log("divHandler", e);
+  }
+
 }
