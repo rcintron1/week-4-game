f124340bf0a9502dbecf68016981d26e79713a02
rcintron1 <rolando.cintron@gmail.com>
2017-08-30 00:22:46 -0400

    adding math logic to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,25 +6,38 @@ $(document).ready(function() {
   var varDiv = "";
   for (i = 0; i < 4; i++) {
     crystalId = "img" + (i + 1);
-    crystal.charPool[i]= crystalId;
+    crystal.pool[i] = crystalId;
     varDiv = $('<div id="' + crystalId + '">');
     varDiv.addClass("col-sm-1");
     link = '<img src="assets/images/' + crystalId + '.jpg">';
     varDiv.append(link);
-    varDiv.on("click",crystal.imgClick);
+    varDiv.on("click", crystal.imgClick);
     // console.log(varDiv);
     $("#div_gems").append(varDiv);
   };
+  $(".btn").on("click", crystal.generateRandomNumbers);
 });
 
 var crystal = {
-  charPool:[],
-  imgClick:function (e){
-    console.log("imageClick",this.id);
+  pool: [],
+  crystalNumber: [],
+  randomNumber: "",
+  imgClick: function(e) {
+    console.log("imageClick", this.id);
     crystal.divHandler(this.id);
   },
-  divHandler:function (e){
+  divHandler: function(e) {
     console.log("divHandler", e);
+  },
+  generateRandomNumbers: function() {
+    console.log("function->generateRandomNumbers")
+    this.crystalNumber = [];
+    while (this.crystalNumber.length < 4) {
+      var x = Math.floor(Math.random() * 12) + 1;;
+      if (!(this.crystalNumber.includes(x))) {
+        this.crystalNumber.push(x);
+      };
+      $("#randomNumber").html(Math.floor(Math.random() * 101) + 19);
+    }
   }
-
 }
