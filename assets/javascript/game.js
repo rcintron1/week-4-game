5be95280518e66444a8632d833cd00539ccfc2cd
Rolando Cintron <rolando.cintron@gmail.com>
2017-08-30 13:59:17 -0400

    added a text file for reference

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,33 @@
+var crystal = {
+  pool: [],
+  crystalNumber: [],
+  randomNumber: "",
+  imgClick: function(e) {
+    console.log("imageClick", this.id);
+    crystal.divHandler(this.id);
+  },
+  divHandler: function(e) {
+    console.log("divHandler", e);
+
+  },
+  generateRandomNumbers: function() {
+    console.log("function->generateRandomNumbers")
+    this.crystalNumber = [];
+    while (this.crystalNumber.length < 4) {
+      var x = Math.floor(Math.random() * 12) + 1;;
+      if (!(this.crystalNumber.includes(x))) {
+        this.crystalNumber.push(x);
+      };
+      $("#randomNumber").html(Math.floor(Math.random() * 101) + 19);
+    }
+  },
+  checkGame: function() {
+
+  },
+}
 $(document).ready(function() {
   console.log("function->ready()")
-  // $("div").html("Rolo");
-  // var varDiv = $("<div></div>");
+  // Loading images using javascript & jquery
   var link = "";
   var varDiv = "";
   for (i = 0; i < 4; i++) {
@@ -15,32 +41,6 @@ $(document).ready(function() {
     // console.log(varDiv);
     $("#div_gems").append(varDiv);
   };
+  // end of loading images using javascript & jquery
   $(".btn").on("click", crystal.generateRandomNumbers);
 });
-
-var crystal = {
-  pool: [],
-  crystalNumber: [],
-  randomNumber: "",
-  imgClick: function(e) {
-    console.log("imageClick", this.id);
-    crystal.divHandler(this.id);
-  },
-  divHandler: function(e) {
-    console.log("divHandler", e);
-  },
-  generateRandomNumbers: function() {
-    console.log("function->generateRandomNumbers")
-    this.crystalNumber = [];
-    while (this.crystalNumber.length < 4) {
-      var x = Math.floor(Math.random() * 12) + 1;;
-      if (!(this.crystalNumber.includes(x))) {
-        this.crystalNumber.push(x);
-      };
-      $("#randomNumber").html(Math.floor(Math.random() * 101) + 19);
-    }
-  },
-  checkGame:function(){
-    
-  }
-}
